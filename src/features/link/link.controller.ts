8bc6f341e248eccb85223843d9c169fc9f2c4146
chenwuai\chenwuai <674939055@qq.com>
2020-08-07 07:55:01 +0800

    ✨feat: 增加新增链接接口

--- src/features/link/link.controller.ts
+++ src/features/link/link.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { LinkService } from './link.service';
 import { LinkEntity } from "./link.entity";
+import { CreateLinkDto } from './dto/link.dto';
 
 @ApiTags('movie')
 @Controller('movie')
@@ -10,6 +11,11 @@ export class LinkController {
 
   async findOne(@Param() {id}):Promise<LinkEntity>{
     return await this.linkService.findOne(id)
+  }
+
+  @Post()
+  async create(@Body() linkData:CreateLinkDto):Promise<any>{
+    return await this.linkService.create(linkData)
   }
 
 }
--- src/features/link/link.service.ts
+++ src/features/link/link.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LinkEntity } from './link.entity';
-import { Repository } from 'typeorm';
+import { Repository, getManager } from 'typeorm';
+import { CreateLinkDto } from './dto/link.dto';
 
 @Injectable()
 export class LinkService {
@@ -11,7 +12,19 @@ export class LinkService {
   ) {}
 
   async findOne(id:number):Promise<LinkEntity>{
-    const movie = this.linkRepository.findOne(id)
-    return movie
+    const link = this.linkRepository.findOne(id)
+    return link
+  }
+
+  async create(linkData:CreateLinkDto):Promise<any>{
+    const movie = getManager().findOne('movie',linkData.movieId)
+    const link = new LinkEntity()
+    link.name = linkData.name
+    link.type = linkData.type
+    link.uri = linkData.uri
+    link.size = linkData.size
+    link.quality = linkData.quality
+    link.movie = movie
+    return await this.linkRepository.save(link)
   }
 }
